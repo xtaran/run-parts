ae7e2ced044f8b4b46fd905f04cc24289a306eb4
Axel Beckert <abe@deuxchevaux.org>
2013-06-07 17:51:18 +0200

    Some reindenting

--- lib/Run/Parts.pm
+++ lib/Run/Parts.pm
@@ -130,9 +130,11 @@ Axel Beckert, C<< <abe@deuxchevaux.org> >>
 
 =head1 BUGS
 
-Please report any bugs or feature requests to C<bug-run-parts at rt.cpan.org>, or through
-the web interface at L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Run-Parts>.  I will be notified, and then you'll
-automatically be notified of progress on your bug as I make changes.
+Please report any bugs or feature requests to C<bug-run-parts at
+rt.cpan.org>, or through the web interface at
+L<http://rt.cpan.org/NoAuth/ReportBug.html?Queue=Run-Parts>.  I will
+be notified, and then you'll automatically be notified of progress on
+your bug as I make changes.
 
 
 
