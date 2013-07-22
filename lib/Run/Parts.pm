adbe6e2552cc9c624bd133e57a46564e7dbfa33f
Axel Beckert <abe@deuxchevaux.org>
2013-07-22 20:48:37 +0200

    Release as 0.06

--- lib/Run/Parts.pm
+++ lib/Run/Parts.pm
@@ -10,11 +10,11 @@ Run::Parts - Offers functionality of Debian's run-parts tool in Perl.
 
 =head1 VERSION
 
-Version 0.05
+Version 0.06
 
 =cut
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 
 use File::Slurp;
 use Run::Parts::Common;
