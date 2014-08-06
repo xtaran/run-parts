3486b6dd216f6726884e7db6faf74c565dbeeaa7
Axel Beckert <abe@deuxchevaux.org>
2014-08-06 12:27:37 +0200

    Bump copyright years

--- lib/Run/Parts/Common.pm
+++ lib/Run/Parts/Common.pm
@@ -87,7 +87,7 @@ You can find documentation for this module with the perldoc command.
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright 2013 Axel Beckert.
+Copyright 2013-2014 Axel Beckert.
 
 This program is free software; you can redistribute it and/or modify it
 under the terms of either: the GNU General Public License as published
